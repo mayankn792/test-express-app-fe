c596c6c68b68e27a0c421f0d04ac14ebdeed569c
Mayank Negi <mayank@Mayanks-MacBook-Air.local>
2024-01-19 23:58:16 +0530

    add auth code

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,10 +4,21 @@ import Link from "next/link";
 export default function Home() {
     async function fun() {
         
-        const response = await fetch('https://test-express-app-flame.vercel.app/u')
-        const users = await response.json()
+        const response0 = await fetch('https://test-express-app-flame.vercel.app/u')
+        const users = await response0.json()
         console.log(users)
-        alert(users);
+        
+        const data = {user: 'John', password: 'JJJJ'}
+        const response = fetch('https://test-express-app-flame.vercel.app/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data),
+        })
+
+        const token = (await response).json()
+        console.log(token)
     }
     return (
       <>
