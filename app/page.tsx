b31282b1d47fd982ded8cf16e2d04e6ee5332864
Mayank Negi <mayank@Mayanks-MacBook-Air.local>
2024-01-19 22:30:30 +0530

    add tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-      <Link href="/login">Login</Link>
+      <Link className='bg-blue-500 text-white font-bold py-2 px-4 rounded' href="/login">Login</Link>
     </>
   )
 }
