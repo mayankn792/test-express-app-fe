f2b68f2a9f2f61732740e49d3f51ac40ef6a9932
Mayank Negi <mayankn792@gmail.com>
2024-01-20 22:02:41 +0530

    add auth endpoint

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,81 +1,98 @@
 'use client';
+import { error } from "console";
 import Link from "next/link";
 import { useRef } from "react";
 
 export default function Home() {
-    async function fun() {
-        
-        const response0 = await fetch('https://test-express-app-flame.vercel.app/u')
-        const users = await response0.json()
-        console.log(users)
-        
-        //auth 
-        const data = {username: 'John', password: 'JJJJ'}
-        const response1 = await fetch('https://test-express-app-flame.vercel.app/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data),
-        })
+  async function fun() {
 
-        const token = await response1.json()
-        console.log(token)
+    const response0 = await fetch('https://test-express-app-flame.vercel.app/u')
+    const users = await response0.json()
+    console.log(users)
 
-        //autherization 
-        const response = await fetch('https://test-express-app-flame.vercel.app/users', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token.access_token
-          },
-        })
+    //auth 
+    const data = { username: 'John', password: 'JJJJ' }
+    const response1 = await fetch('https://test-express-app-flame.vercel.app/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    })
 
-        const user = await response.json()
-        console.log(user)
-    }
-    const usernameRef = useRef(null);
-    const passwordRef = useRef(null);
-    
-    const handleSumbit = (event: any) => {
-      event.preventDefault()
-      var user: string = ''
-      var pass: string = ''
-      
-      if (usernameRef.current) {
-        user = (usernameRef.current as {value: string}).value;
-      }
+    const token = await response1.json()
+    console.log(token)
 
-      if (passwordRef.current) {
-        pass = (passwordRef.current as {value: string}).value;
-      }
+    //autherization 
+    const response = await fetch('https://test-express-app-flame.vercel.app/users', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token.access_token
+      },
+    })
 
-      console.log(user)
-      console.log(pass)
-      
+    const user = await response.json()
+    console.log(user)
+  }
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const handleSumbit = (event: any) => {
+    event.preventDefault()
+    var user: string = ''
+    var pass: string = ''
+
+    if (usernameRef.current) {
+      user = (usernameRef.current as { value: string }).value;
     }
 
-    return (
-      <>
-        <form onSubmit={handleSumbit}>
-          <input
-            type="text"
-            ref={usernameRef}
-            placeholder="Enter username"
-          />
+    if (passwordRef.current) {
+      pass = (passwordRef.current as { value: string }).value;
+    }
 
-          <input
-            type="text"
-            ref={passwordRef}
-            placeholder="Enter password"
-          />
+    console.log(user)
+    console.log(pass)
 
-          <button type="submit">Submit</button>
-        </form>
-        
-        {/* <input placeholder="Enter Username"></input>
+    const data = { username: user, password: pass }
+    fetch('https://test-express-app-flame.vercel.app/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSumbit}>
+        <input
+          type="text"
+          ref={usernameRef}
+          placeholder="Enter username"
+        />
+
+        <input
+          type="text"
+          ref={passwordRef}
+          placeholder="Enter password"
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+
+      {/* <input placeholder="Enter Username"></input>
         <input placeholder="Enter Password"></input>
         <Link href="#" onClick={() => fun()}>Login</Link> */}
-      </>
-    )
-  }
+    </>
+  )
+}
